8913962bb0a8768d28b08f4e50a013582e41d4c1
William Badger <wkbadger@gmail.com>
2021-06-15 13:53:48 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function superbowlWin(myArray) {
+  let yearWin = myArray.find(element => element.result === "W")
+  if (yearWin)
+    return yearWin.year
+}
+superbowlWin(record)
 
+/*
+function superbowlWin(array){
+  let record = array.find((season)=>{
+    return (season.result === "W")
+  })
+  if (record){
+    return record.year
+  }
+}
+*/
